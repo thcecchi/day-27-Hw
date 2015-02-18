b0e6542f8d92dbf90d29ef6e7ec90f31420c6f80
Tyler Cecchi <thcecchi@gmail.com>
2015-02-18 09:41:10 -0500

    style updates

--- router.js
+++ router.js
@@ -1,28 +1,28 @@
-// //define router class
-// var MovieRouter = Backbone.Router.extend ({
-//
-//   routes: {
-//     '' : "home",
-//     'movie/:name': 'movieName'
-//   },
-//
-//   home: function () {
-//     // alert('you are viewing home page');
-//     // $('.wrapper').show();
-//
-//   },
-//
-//   movieName: function (name) {
-//     // alert('you are viewing a movie');
-//     // $('.wrapper').hide();
-//   }
-//
-//
-// });
-//
-// //define our new instance of router
-// var appRouter = new MovieRouter();
-//
-// // use html5 History API
-//
-// Backbone.history.start({pushState: true});
+//define router class
+var MovieRouter = Backbone.Router.extend ({
+
+  routes: {
+    '' : "home",
+    'movie/:name': 'movieName'
+  },
+
+  home: function () {
+    // alert('you are viewing home page');
+    $('.wrapper').show();
+
+  },
+
+  movieName: function (name) {
+    // alert('you are viewing a movie');
+    $('.wrapper').hide();
+  }
+
+
+});
+
+//define our new instance of router
+var appRouter = new MovieRouter();
+
+// use html5 History API
+
+Backbone.history.start({pushState: true});
